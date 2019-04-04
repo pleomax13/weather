df5a5d1d68001db962be3ec43d0dcd1c102f718f
pleomax13 <43319882+pleomax13@users.noreply.github.com>
2019-04-04 14:40:08 +0300

    Add files via upload

---
+++ precache-manifest.81b446b177d392566f3aacf98df8f36a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "20d67c22afd0e6318ca5",
+    "url": "/weather/static/css/main.63359d7c.chunk.css"
+  },
+  {
+    "revision": "20d67c22afd0e6318ca5",
+    "url": "/weather/static/js/main.20d67c22.chunk.js"
+  },
+  {
+    "revision": "5346c2795f64f3102fab",
+    "url": "/weather/static/js/runtime~main.5346c279.js"
+  },
+  {
+    "revision": "ff686350ab78b2148ac4",
+    "url": "/weather/static/js/2.ff686350.chunk.js"
+  },
+  {
+    "revision": "980ae7d4f472e9c0781c20abf613bf21",
+    "url": "/weather/static/media/loadScreen.980ae7d4.jpg"
+  },
+  {
+    "revision": "927a63ccf526f9ab4530ff0d036e5752",
+    "url": "/weather/static/media/rain.927a63cc.jpg"
+  },
+  {
+    "revision": "9f3392578b2154c398238f55de17d28d",
+    "url": "/weather/static/media/thunderstorm.9f339257.jpg"
+  },
+  {
+    "revision": "a0e014550bae70a20538bffc77909577",
+    "url": "/weather/static/media/fog.a0e01455.jpg"
+  },
+  {
+    "revision": "12df609b80f93198a5fa44f0a06aa63f",
+    "url": "/weather/static/media/snow.12df609b.jpg"
+  },
+  {
+    "revision": "c2e8dca6f1712cc96fe777412c45a983",
+    "url": "/weather/static/media/clear.c2e8dca6.jpg"
+  },
+  {
+    "revision": "914cfd9dbd3ad857bcfbee8d29eca521",
+    "url": "/weather/static/media/clouds.914cfd9d.jpg"
+  },
+  {
+    "revision": "d2d5eda3e44f5d445a9aa8ed8367f219",
+    "url": "/weather/index.html"
+  }
+];
